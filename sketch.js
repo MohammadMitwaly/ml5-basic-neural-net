870de09f56ccffcfa5a7ca0d65625ff46bdef24b
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-05-20 20:58:02 +0300

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,83 @@
+let model;
+let modelOptions = {
+  inputs: ["x", "y"],
+  outputs: ["label"],
+  task: "classification",
+  debug: "true", // Make this toggleable by the user
+};
+let targetNotation = "C";
+let trainingBtn;
+let programState = "collection";
+
+const modelIsTraining = (epoch, loss) => {
+  console.log(
+    `This is the ${epoch} epoch, the current loss function value is ${loss}`
+  );
+};
+
+const modelFinishedTraining = () => {
+  console.log(`Model finished training!`);
+  programState = "prediction";
+};
+
+const predictionResults = (error, results) => {
+  if (error) {
+    console.error("Failed with the follwing error", error);
+    return;
+  }
+  console.log("Prediction result:", results);
+};
+
+function setup() {
+  createCanvas(800, 800);
+  background(0);
+  model = ml5.neuralNetwork(modelOptions);
+  trainingBtn = createButton("Train the model!");
+  trainingBtn.position(0, 0);
+  trainingBtn.mousePressed(trainModelWithUserData);
+}
+
+function trainModelWithUserData() {
+  let trainingOptions = {
+    epochs: 100,
+  };
+  model.normalizeData();
+  programState = "training";
+  model.train(trainingOptions, modelIsTraining, modelFinishedTraining);
+}
+
+function keyPressed() {
+  if (programState === "collection") {
+    if (key === "d" || key === "e" || key === "c") {
+      targetNotation = key.toUpperCase();
+    }
+  }
+}
+
+function mousePressed() {
+  let xCord = mouseX;
+  let yCord = mouseY;
+
+  // Object representing one data point in this data-set
+  let dataPoint = {
+    x: xCord,
+    y: yCord,
+  };
+  // The correct label attribute to be used in training
+  let correctLabel = {
+    label: targetNotation,
+  };
+
+  if (programState === "collection") {
+    model.addData(dataPoint, correctLabel);
+    stroke(255);
+    noFill();
+    ellipse(mouseX, mouseY, 24);
+    fill(255);
+    noStroke();
+    textAlign(CENTER, CENTER);
+    text(targetNotation, mouseX, mouseY);
+  } else if (programState === "prediction") {
+    model.classify(dataPoint, predictionResults);
+  }
+}
